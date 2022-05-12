0acc664e83fd1f3f0dc31ac393e7989bb2b9fd67
ryoma kawahara <ryoma.k.0224@gmail.com>
2022-05-12 20:19:57 +0900

    refactor with first class collection

--- oidc/id_token.go
+++ oidc/id_token.go
@@ -1,18 +1,8 @@
 package oidc
 
 import (
-	"crypto"
-	"crypto/rsa"
-	"crypto/sha256"
-	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"math/big"
-	"net/http"
-	"net/url"
 	"sns-login/model"
 	"strings"
 
@@ -38,19 +28,6 @@ type header struct {
 	Alg string `json:"alg"`
 	Kid string `json:"kid"`
 	Typ string `json:"typ"`
-}
-
-type jwks struct {
-	Keys []jwk `json:"keys"`
-}
-
-type jwk struct {
-	E   string `json:"e"`
-	Kty string `json:"kty"`
-	Kid string `json:"kid"`
-	Use string `json:"use"`
-	N   string `json:"n"`
-	Alg string `json:"alg"`
 }
 
 var (
@@ -82,76 +59,6 @@ func NewIdToken(rawToken string) (*idToken, error) {
 	idToken.header = *header
 
 	return idToken, nil
-}
-
-func (token idToken) ValidateSignature(jwksUrl string) error {
-	key, err := token.getJwk(jwksUrl)
-	if err != nil {
-		return err
-	}
-
-	byteN, err := base64.RawURLEncoding.DecodeString(key.N)
-	if err != nil {
-		return fmt.Errorf("failed to decode base64 modulus: %w", err)
-	}
-
-	const standardExponent = 65537
-	pubKey := &rsa.PublicKey{
-		N: new(big.Int).SetBytes(byteN),
-		E: standardExponent, // TODO: key.E -> "AQAB"から導きたい
-	}
-
-	headerAndPayload := fmt.Sprintf("%s.%s", token.rawHeader, token.RawPayload)
-	sha := sha256.New()
-	sha.Write([]byte(headerAndPayload))
-
-	decSignature, err := base64.RawURLEncoding.DecodeString(token.rawSignature)
-	if err != nil {
-		return fmt.Errorf("failed to base64 decode id_token signature: %w", err)
-	}
-
-	if err := rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, sha.Sum(nil), decSignature); err != nil {
-		return fmt.Errorf("failed to verify id_token signature: %w", err)
-	}
-
-	return nil
-}
-
-func (token idToken) getJwk(jwksUrl string) (jwk, error) {
-	parsedUrl, err := url.Parse(jwksUrl)
-	if err != nil {
-		return jwk{}, fmt.Errorf("failed to parse jwks url: %w", err)
-	}
-
-	resp, err := http.Get(parsedUrl.String())
-	if err != nil {
-		return jwk{}, fmt.Errorf("failed to GET JWKs endpoint: %w", err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(resp.Body)
-	byteArray, _ := ioutil.ReadAll(resp.Body)
-	keys := &jwks{}
-	if err := json.Unmarshal(byteArray, keys); err != nil {
-		return jwk{}, fmt.Errorf("failed to unmarshal JWKs response: %w", err)
-	}
-
-	var key jwk
-	var isFound bool
-	for _, v := range keys.Keys {
-		if token.header.Kid == v.Kid {
-			key = v
-			isFound = true
-		}
-	}
-	if !isFound {
-		return jwk{}, errJwkNotFound
-	}
-
-	return key, nil
 }
 
 func IssToIdProvider(iss string) (model.IdProvider, error) {
---
+++ oidc/jwk.go
@@ -0,0 +1,108 @@
+package oidc
+
+import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"net/url"
+)
+
+type jwks struct {
+	Keys []jwk `json:"keys"`
+}
+
+type jwk struct {
+	E   string `json:"e"`
+	Kty string `json:"kty"`
+	Kid string `json:"kid"`
+	Use string `json:"use"`
+	N   string `json:"n"`
+	Alg string `json:"alg"`
+}
+
+func (token idToken) ValidateSignature(jwksUrl string) error {
+	key, err := token.getJwk(jwksUrl)
+	if err != nil {
+		return err
+	}
+
+	byteN, err := base64.RawURLEncoding.DecodeString(key.N)
+	if err != nil {
+		return fmt.Errorf("failed to decode base64 modulus: %w", err)
+	}
+
+	const standardExponent = 65537
+	pubKey := &rsa.PublicKey{
+		N: new(big.Int).SetBytes(byteN),
+		E: standardExponent, // TODO: key.E -> "AQAB"から導きたい
+	}
+
+	headerAndPayload := fmt.Sprintf("%s.%s", token.rawHeader, token.RawPayload)
+	sha := sha256.New()
+	sha.Write([]byte(headerAndPayload))
+
+	decSignature, err := base64.RawURLEncoding.DecodeString(token.rawSignature)
+	if err != nil {
+		return fmt.Errorf("failed to base64 decode id_token signature: %w", err)
+	}
+
+	if err := rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, sha.Sum(nil), decSignature); err != nil {
+		return fmt.Errorf("failed to verify id_token signature: %w", err)
+	}
+
+	return nil
+}
+
+func (token idToken) getJwk(jwksUrl string) (jwk, error) {
+	parsedUrl, err := url.Parse(jwksUrl)
+	if err != nil {
+		return jwk{}, fmt.Errorf("failed to parse jwks url: %w", err)
+	}
+
+	resp, err := http.Get(parsedUrl.String())
+	if err != nil {
+		return jwk{}, fmt.Errorf("failed to GET JWKs endpoint: %w", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	keys := &jwks{}
+	if err := json.Unmarshal(byteArray, keys); err != nil {
+		return jwk{}, fmt.Errorf("failed to unmarshal JWKs response: %w", err)
+	}
+
+	foundKey, err := keys.find(token.header.Kid)
+	if err != nil {
+		return jwk{}, err
+	}
+
+	return foundKey, nil
+}
+
+func (keys jwks) find(kid string) (jwk, error) {
+	var foundKey jwk
+	for _, key := range keys.Keys {
+		if key.Kid == kid {
+			foundKey = key
+
+			break
+		}
+	}
+
+	if foundKey != (jwk{}) {
+		return foundKey, nil
+	} else {
+		return jwk{}, errJwkNotFound
+	}
+}
